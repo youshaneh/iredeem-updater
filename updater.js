c689df1f1998c5f8cdfc0e75acfcead3cdd25b9f
Shane Huang <youshaneh@gmail.com>
2020-05-24 22:22:53 +0800

    feat: continue from where we left off after a restart

--- updater.js
+++ updater.js
@@ -4,51 +4,51 @@ import { updateDB, initBrowserPage, getRequestParameters, setupAndGetRequestPara
 import { getIRedeemRepository } from './db.js';
 
 dotenv.config();
-let airports = JSON.parse(fs.readFileSync('available-airports.json', 'utf-8'));
-let catchUp = '';  //TODO: continue from where we left off
-(async function update(){
-  try{
+let routes = JSON.parse(fs.readFileSync('routes.json', 'utf-8'));
+let resume = false;
+let lastRoute;
+(async function update() {
+  try {
     let [iRedeemRepository] = await Promise.all([
       getIRedeemRepository(),
       initBrowserPage().then(setupAndGetRequestParameters)]);
     while (true) {
       let today = new Date();
-      let from = 'HKG';
-      for (let airport of airports) {
-        let to = airport[2];
-        if (catchUp) {
-          if (to != catchUp) {
-            continue;
+      for (let route of routes) {
+        for (let { from, to } of [{ from: route[0], to: route[1] }, { from: route[1], to: route[0] }]) {
+          for (let i = 56; i <= 60; i++) {
+            let date = new Date(today.getTime() + i * 24 * 60 * 60 * 1000);
+            if (resume) {
+              if (from != lastRoute.from || to != lastRoute.to || date < lastRoute.date) continue;
+              resume = false;
+            }
+            lastRoute = { from, to, date };
+            date = date.toISOString().split('T')[0];
+            let flights;
+            try {
+              flights = await getFlights(from, to, date);
+            }
+            catch (e) {
+              if (e.message != 'Request quota is used up') console.error(e);
+              await getRequestParameters();
+              flights = await getFlights(from, to, date);
+            }
+            if (process.env.NODE_ENV == 'dev') {
+              let testDataDir = `test_data/`;
+              if (process.env.NODE_ENV == 'dev' && !fs.existsSync(testDataDir)) fs.mkdirSync(testDataDir);
+              fs.writeFileSync(`${testDataDir}/${from}_${to}_${date}_flights.json`, JSON.stringify(flights));
+            }
+            await updateDB(from, to, date, flights, iRedeemRepository);
+            await new Promise(resolve => setTimeout(resolve, 1000));
           }
-          else {
-            catchUp = null;
-          }
-        }
-        for (let i = 3; i <= 60; i++) {
-          let date = new Date(today.getTime() + i * 24 * 60 * 60 * 1000).toISOString().split('T')[0];
-          let flights;
-          try {
-            flights = await getFlights(from, to, date); //TODO: get return trip flights
-          }
-          catch (e) {
-            if(e.message != 'Request quota is used up') console.error(e);
-            await getRequestParameters();
-            flights = await getFlights(from, to, date);
-          }
-          if (process.env.NODE_ENV == 'dev') {
-            let testDataDir = `test_data/`;
-            if (process.env.NODE_ENV == 'dev' && !fs.existsSync(testDataDir)) fs.mkdirSync(testDataDir);
-            fs.writeFileSync(`${testDataDir}/${from}_${to}_${date}_flights.json`, JSON.stringify(flights));
-          }
-          await updateDB(from, to, date, flights, iRedeemRepository);
-          await new Promise(resolve => setTimeout(resolve, 1000));
         }
       }
+      //TODO: remove outdated recoreds in DB at the end
     }
-    //TODO: remove redundant recoreds in DB at the end
   }
-  catch(e){
+  catch (e) {
     console.error(e);
+    resume = lastRoute != undefined;
     update();
   }
 })();
