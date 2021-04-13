0a7f0d5a3f7d2cae8511745aaeb36038b992d1c5
You-Sheng <youshaneh@gmail.com>
2021-04-13 14:02:51 +0800

    feat: delete outdated data from db at the beginning of each run

--- db.js
+++ db.js
@@ -1,210 +1,222 @@
 import mysql from 'mysql';
 
 class IRedeemRepository {
-  constructor(connection) {
-    this.connection = connection;
-  }
-
-  addFlight({ airline, flight_number, aircraft,
-    status_f, status_b, status_n, status_r,
-    departure_airport, departure_terminal, departure_time,
-    arrival_airport, arrival_terminal, arrival_time }) {
-    return new Promise((resolve, reject) => {
-      this.connection.query(`INSERT INTO flight(
-        airline, flight_number, aircraft,
-        status_f, status_b, status_n, status_r,
-        departure_airport, departure_terminal, departure_time,
-        arrival_airport, arrival_terminal, arrival_time)
-        VALUES(?,?,?,?,?,?,?,
-          ?,?,?,
-          ?,?,?);`,
-        [airline, flight_number, aircraft,
-          status_f, status_b, status_n, status_r,
-          departure_airport, departure_terminal, departure_time,
-          arrival_airport, arrival_terminal, arrival_time],
-        (error, result) => {
-          if (error) {
-            reject(error);
-          }
-          else {
-            resolve(result);
-          }
-        }
-      );
-    });
-  }
-
-  addHistory({ id: flight_id, update_time,
-    status_f, status_b, status_n, status_r }) {
-    return new Promise((resolve, reject) => {
-      this.connection.query(`INSERT INTO history(
-        flight_id, update_time,
-        status_f, status_b, status_n, status_r)
-        VALUES(?,?,?,?,?,?);`,
-        [flight_id, update_time,
-          status_f, status_b, status_n, status_r],
-        (error, result) => {
-          if (error) {
-            reject(error);
-          }
-          else {
-            resolve(result);
-          }
-        }
-      );
-    });
-  }
-
-  isStatusChanged(currentStatus, lastStatus) {
-    return (currentStatus.status_f != lastStatus.status_f) ||
-      (currentStatus.status_b != lastStatus.status_b) ||
-      (currentStatus.status_n != lastStatus.status_n) ||
-      (currentStatus.status_r != lastStatus.status_r);
-  }
-
-  async updateInterval(from, to, date, itineraries) {
-    this.connection.beginTransaction(function (err) {
-      if (err) { throw err; }
-    });
-    try {
-      await this.removeItineraries(from, to, date);
-      await this.addItinerarys(itineraries);
+    constructor(connection) {
+        this.connection = connection;
     }
-    catch (e) {
-      this.connection.rollback(() => { throw e });
-    }
-    this.connection.commit(function (err) {
-      if (err) return connection.rollback(() => { throw err });
-    });
-  }
 
-  removeItineraries(from, to, date) {
-    return new Promise((resolve, reject) => {
-      this.connection.query(`DELETE itinerary FROM itinerary
-          JOIN flight AS f1 ON itinerary.flight1 = f1.id
-          LEFT JOIN flight AS f2 ON itinerary.flight2 = f2.id
-        WHERE
-          f1.departure_airport = ?
-          AND ((f1.arrival_airport = ? AND f2.departure_airport IS NULL)
-          OR f2.arrival_airport = ?)
-          AND f1.departure_time >= ?
-          AND f1.departure_time < DATE_ADD(?, INTERVAL 1 DAY);`,
-        [from, to, to, date, date],
-        (error, result) => {
-          if (error) {
-            reject(error);
-          }
-          else {
-            resolve(result);
-          }
-        }
-      );
-    })
-  }
-
-  async addItinerarys(itineraries) {
-    for (let i = 0; i < itineraries.length; i++) {
-      await this.addItinerary(itineraries[i]);
-    }
-  }
-
-  async addItinerary([flight1, flight2]) {
-    let flightId1 = await this.insertOrUpdateFlight(flight1);
-    let flightId2 = flight2 ? await this.insertOrUpdateFlight(flight2) : undefined;
-    return new Promise((resolve, reject) => {
-      this.connection.query(`INSERT INTO itinerary(flight1, flight2) VALUES(?,?);`,
-        [flightId1, flightId2],
-        (error, result) => {
-          if (error) {
-            reject(error);
-          }
-          else {
-            resolve(result);
-          }
-        }
-      );
-    })
-  }
-
-  async insertOrUpdateFlight(flight) {
-    let storedFlight = await this.getFlight(flight);
-    if (!storedFlight) {
-      return this.addFlight(flight).then(flight => flight.insertId);
-    }
-    else {
-      if (this.isStatusChanged(flight, storedFlight)) await this.addHistory(storedFlight);
-      await this.updateFlightStatus(flight);
-    }
-    return storedFlight.id;
-  }
-
-  updateFlightStatus({ airline, flight_number, departure_time,
-    status_f, status_b, status_n, status_r }) {
-    return new Promise((resolve, reject) => {
-      this.connection.query(`UPDATE flight SET
-        status_f = ?, status_b = ?, status_n = ?, status_r = ?
-        WHERE airline = ? AND flight_number = ?
-        AND departure_time = ?;`,
-        [status_f, status_b, status_n, status_r,
-          airline, flight_number, departure_time],
-        (error, result) => {
-          if (error) {
-            reject(error);
-          }
-          else {
-            resolve();
-          }
-        }
-      );
-    });
-  }
-
-  getFlight({ airline, flight_number, departure_time }) {
-    return new Promise((resolve, reject) => {
-      this.connection.query(`SELECT * from flight
-        where airline = ? AND flight_number = ? AND
-        departure_time = ?`,
-        [airline, flight_number, departure_time],
-        (error, result) => {
-          if (error) {
-            reject(error);
-          }
-          else {
-            resolve(result[0]);
-          }
+    async deleteOutdatedData() {
+        await new Promise((resolve, reject) => {
+            this.connection.query(`DELETE itinerary FROM itinerary
+                JOIN flight AS f1 ON itinerary.flight1 = f1.id
+                WHERE f1.departure_time < CURRENT_DATE();`, (error, result) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            });
         });
-    });
-  }
+        await new Promise((resolve, reject) => {
+            this.connection.query(`DELETE FROM flight WHERE departure_time < CURRENT_DATE();`, (error, result) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    }
 
-  end() {
-    this.connection.end();
-  }
+    async updateInterval(from, to, date, itineraries) {
+        await new Promise((resolve, reject) => {
+            this.connection.beginTransaction(function (err) {
+                if (err) reject(err);
+                resolve();
+            });
+        });
+
+        try {
+            await this.removeItineraries(from, to, date);
+            await this.addItinerarys(itineraries);
+        } catch (e) {
+            this.connection.rollback(() => {
+                throw e
+            });
+        }
+
+        await new Promise((resolve, reject) => {
+            this.connection.commit(function (err) {
+                if (err) {
+                    connection.rollback(() => {
+                        reject(err);
+                    })
+                }
+                resolve();
+            });
+        });
+    }
+
+    removeItineraries(from, to, date) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(`DELETE itinerary FROM itinerary
+                JOIN flight AS f1 ON itinerary.flight1 = f1.id
+                LEFT JOIN flight AS f2 ON itinerary.flight2 = f2.id
+                WHERE
+                f1.departure_airport = ?
+                AND ((f1.arrival_airport = ? AND f2.departure_airport IS NULL)
+                OR f2.arrival_airport = ?)
+                AND f1.departure_time >= ?
+                AND f1.departure_time < DATE_ADD(?, INTERVAL 1 DAY);`, [from, to, to, date, date],
+                (error, result) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(result);
+                    }
+                }
+            );
+        })
+    }
+
+    async addItinerarys(itineraries) {
+        for (let i = 0; i < itineraries.length; i++) {
+            await this.addItinerary(itineraries[i]);
+        }
+    }
+
+    async addItinerary([flight1, flight2]) {
+        let flightId1 = await this.insertOrUpdateFlight(flight1);
+        let flightId2 = flight2 && await this.insertOrUpdateFlight(flight2);
+        return new Promise((resolve, reject) => {
+            this.connection.query(`INSERT INTO itinerary(flight1, flight2) VALUES(?,?);`, [flightId1, flightId2],
+                (error, result) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(result);
+                    }
+                }
+            );
+        })
+    }
+
+    async insertOrUpdateFlight(flight) {
+        let storedFlight = await this.getFlight(flight);
+        if (storedFlight) {
+            await this.updateFlightStatus(flight);
+            return storedFlight.id;
+        } else {
+            let result = await this.addFlight(flight);
+            return result.insertId;
+        }
+    }
+
+    updateFlightStatus({
+        airline,
+        flight_number,
+        departure_time,
+        status_f,
+        status_b,
+        status_n,
+        status_r
+    }) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(`UPDATE flight SET
+                status_f = ?, status_b = ?, status_n = ?, status_r = ?
+                WHERE airline = ? AND flight_number = ?
+                AND departure_time = ?;`, [status_f, status_b, status_n, status_r,
+                airline, flight_number, departure_time
+            ],
+                (error, result) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve();
+                    }
+                }
+            );
+        });
+    }
+
+    addFlight({
+        airline,
+        flight_number,
+        aircraft,
+        status_f,
+        status_b,
+        status_n,
+        status_r,
+        departure_airport,
+        departure_terminal,
+        departure_time,
+        arrival_airport,
+        arrival_terminal,
+        arrival_time
+    }) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(`INSERT INTO flight(
+                airline, flight_number, aircraft,
+                status_f, status_b, status_n, status_r,
+                departure_airport, departure_terminal, departure_time,
+                arrival_airport, arrival_terminal, arrival_time)
+                VALUES(?,?,?,?,?,?,?,
+                ?,?,?,
+                ?,?,?);`, [airline, flight_number, aircraft,
+                status_f, status_b, status_n, status_r,
+                departure_airport, departure_terminal, departure_time,
+                arrival_airport, arrival_terminal, arrival_time], (error, result) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(result);
+                    }
+                }
+            );
+        });
+    }
+
+    getFlight({
+        airline,
+        flight_number,
+        departure_time
+    }) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(`SELECT * from flight
+                where airline = ? AND flight_number = ? AND
+                departure_time = ?`, [airline, flight_number, departure_time],
+                (error, result) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(result[0]);
+                    }
+                });
+        });
+    }
+
+    end() {
+        this.connection.end();
+    }
 }
 
-export async function getIRedeemRepository(retry = 5) {
-  try {
+export async function getIRedeemRepository() {
     let connection = mysql.createConnection({
-      host: 'localhost',
-      port: 3306,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: 'iredeem'
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: 'iredeem'
     });
-    return await new Promise((resolve, reject) => {
-      connection.connect(e => {
-        if (e) {
-          reject(e);
-        }
-        else {
-          console.info('connected to database');
-          resolve(new IRedeemRepository(connection));
-        }
-      });
+    return new Promise((resolve, reject) => {
+        connection.connect(e => {
+            if (e) {
+                console.error(e);
+                reject(e);
+            } else {
+                console.info('connected to database');
+                resolve(new IRedeemRepository(connection));
+            }
+        });
     });
-  }
-  catch (e) {
-    if (retry < 0) throw e;
-    console.error(e);
-    return await getIRedeemRepository(retry - 1);
-  }
 }
--- test-script.js
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import shelljs from 'shelljs';
-
-let esm = fs.readFileSync('package.json', 'utf-8');
-let cmj = JSON.stringify(JSON.parse(esm, (key, value) => (key == 'type')? 'commonjs' : value));
-fs.writeFileSync('package.json', cmj);
-shelljs.exec('npx jest --colors');
-fs.writeFileSync('package.json', esm);
